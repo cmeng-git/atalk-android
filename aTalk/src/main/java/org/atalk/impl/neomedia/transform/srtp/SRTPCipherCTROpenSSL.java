cb19e65ea2ba395fcfb3d6e7ec3c5806760372ce
Milan Kral <milan.kral@azet.sk>
2020-02-28 21:12:14 +0100

    AES-256 acceleration using OpenSSL
    AES-256 acceleration is implemented in the same way as AES-128 acceleration

--- aTalk/src/main/java/org/atalk/impl/neomedia/transform/srtp/SRTPCipherCTROpenSSL.java
+++ aTalk/src/main/java/org/atalk/impl/neomedia/transform/srtp/SRTPCipherCTROpenSSL.java
@@ -21,26 +21,34 @@ package org.atalk.impl.neomedia.transform.srtp;
  */
 public class SRTPCipherCTROpenSSL extends SRTPCipherCTR
 {
-    private static native long AES128CTR_CTX_create();
+    private static native long AES_CTR_CTX_create();
 
-    private static native void AES128CTR_CTX_destroy(long ctx);
+    private static native void AES_CTR_CTX_destroy(long ctx);
 
     private static native boolean AES128CTR_CTX_init(long ctx, byte[] key);
 
     private static native boolean AES128CTR_CTX_process(long ctx, byte[] iv,
             byte[] inOut, int offset, int len);
 
+    private static native boolean AES256CTR_CTX_init(long ctx, byte[] key);
+
+    private static native boolean AES256CTR_CTX_process(long ctx, byte[] iv,
+                                                        byte[] inOut, int offset, int len);
+
+
+    private int key_length = -1;
+
     /**
-     * the OpenSSL AES128CTR context
+     * the OpenSSL AES128CTR / AES256 context
      */
-    private long ctx;
+    private long ctx = 0;
 
     public SRTPCipherCTROpenSSL()
     {
         if (!OpenSSLWrapperLoader.isLoaded())
             throw new RuntimeException("OpenSSL wrapper not loaded");
 
-        ctx = AES128CTR_CTX_create();
+        ctx = AES_CTR_CTX_create();
         if (ctx == 0)
             throw new RuntimeException("CIPHER_CTX_create");
     }
@@ -50,11 +58,22 @@ public class SRTPCipherCTROpenSSL extends SRTPCipherCTR
      */
     public void init(byte[] key)
     {
-        if (key.length != BLKLEN)
-            throw new IllegalArgumentException("key.length != BLKLEN");
+        switch (key.length)
+        {
+            case 16:
+                if (!AES128CTR_CTX_init(ctx, key))
+                    throw new RuntimeException("AES128CTR_CTX_init");
+                break;
 
-        if (!AES128CTR_CTX_init(ctx, key))
-            throw new RuntimeException("AES128CTR_CTX_init");
+            case 32:
+                if (!AES256CTR_CTX_init(ctx, key))
+                    throw new RuntimeException("AES256CTR_CTX_init");
+                break;
+            default:
+                throw new IllegalArgumentException("Only AES128 and AES256 is supported");
+        }
+
+        key_length = key.length;
     }
 
     /**
@@ -69,7 +88,7 @@ public class SRTPCipherCTROpenSSL extends SRTPCipherCTR
             // anyway. There is no way to explicitly invoke the destroying at
             // the time of this writing but it is a start.
             if (ctx != 0) {
-                AES128CTR_CTX_destroy(ctx);
+                AES_CTR_CTX_destroy(ctx);
                 ctx = 0;
             }
         } finally {
@@ -84,7 +103,20 @@ public class SRTPCipherCTROpenSSL extends SRTPCipherCTR
     {
         checkProcessArgs(data, off, len, iv);
 
-        if (!AES128CTR_CTX_process(ctx, iv, data, off, len))
-            throw new RuntimeException("AES128CTR_CTX_process");
+        switch (key_length)
+        {
+            case 16:
+                if (!AES128CTR_CTX_process(ctx, iv, data, off, len))
+                    throw new RuntimeException("AES128CTR_CTX_process");
+                break;
+
+            case 32:
+                if (!AES256CTR_CTX_process(ctx, iv, data, off, len))
+                    throw new RuntimeException("AES256CTR_CTX_process");
+                break;
+
+            default:
+                throw new IllegalArgumentException("Only AES128 and AES256 is supported");
+        }
     }
 }
