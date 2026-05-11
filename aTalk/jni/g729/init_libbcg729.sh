cc956e5d7e3a4f11d9cface1477989fea38415f5
cmeng <cmeng.gm@gmail.com>
2026-05-12 00:07:16 +0800

    Update init_libbcg729.sh
    Replace invalid bcg729 lib download link.

--- aTalk/jni/g729/init_libbcg729.sh
+++ aTalk/jni/g729/init_libbcg729.sh
@@ -35,10 +35,8 @@ fi
 
 rm -rf ${LIB_BCG729}
 echo -e "\n================ Fetching library source for ${LIB_BCG729}: ${LIB_BCG729}-${LIB_BCG729_VER} ============================"
-# git clone https://gitlab.linphone.org/BC/public/bcg729.git
-# wget -O- https://gitlab.linphone.org/BC/public/bcg729/-/archive/release/1.1.1/bcg729-release-1.1.1.tar.gz | tar xz --strip-components=1 --one-top-level=${LIB_BCG729}
-wget --no-check-certificate -O- https://gitlab.linphone.org/BC/public/bcg729/-/archive/${LIB_BCG729_GIT}.tar.gz  | tar xz --strip-components=1 --one-top-level=${LIB_BCG729}
-# wget -O- https://codeload.github.com/BelledonneCommunications/bcg729/tar.gz/refs/tags/${LIB_BCG729_VER}  | tar xz --strip-components=1 --one-top-level=${LIB_BCG729}
+# invalid: wget --no-check-certificate -O- https://gitlab.linphone.org/BC/public/bcg729/-/archive/${LIB_BCG729_GIT}.tar.gz  | tar xz --strip-components=1 --one-top-level=${LIB_BCG729}
+wget --no-check-certificate -O- https://github.com/BelledonneCommunications/bcg729/archive/refs/tags/${LIB_BCG729_GIT}.tar.gz  | tar xz --strip-components=1 --one-top-level=${LIB_BCG729}
 
-pushd ${LIB_BCG729} || exit
+pushd ${LIB_BCG729} || exit 1
 echo -e "======== Completed opus library source update ============================"
